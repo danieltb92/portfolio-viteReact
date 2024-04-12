b0a75484c5d6ca11252cead4685266adb8c329b1
danieltb92 <daniel.tunjano92@gmail.com>
2024-04-12 12:56:37 -0500

    Skipped NotionmdToReact-Service

--- client/src/services/NotionmdToReact-Service.jsx
+++ client/src/services/NotionmdToReact-Service.jsx
@@ -1,36 +1,37 @@
-import { useState, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown'; // Librería react-markdown
+// import { useState, useEffect } from 'react';
+// import { Switch, Route } from 'react-router-dom';
+// import ReactMarkdown from 'react-markdown'; 
 
-const App = () => {
-  const [markdownData, setMarkdownData] = useState([]);
+// // Funcion para renderizar markdown a React
+// const App = () => {
+//   const [markdownData, setMarkdownData] = useState([]);
+//   // useEffect para obtener de la URL el JSON con el Markdown de las paginas 
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await fetch('http://localhost:4000/pages-md');
+//       const data = await response.json();
+//       setMarkdownData(data);
+//     }
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('http://localhost:4000/pages-md');
-      const data = await response.json();
-      setMarkdownData(data);
-    }
+//     fetchData();
+//   }, []);
 
-    fetchData();
-  }, []);
+//   return (
+//       <Switch>
+//         {markdownData.map((item) => (
+//           <Route
+//             key={item.id}
+//             path={`/page/${item.slug}`}
+//             render={() => (
+//               <div key={item.id}>
+//                 {/* <h2>{item.title}</h2> */}
+//                 <ReactMarkdown>{item.content}</ReactMarkdown>
+//               </div>
+//             )}
+//           />
+//         ))}
+//       </Switch>
+//   );
+// };
 
-  return (
-      <Switch>
-        {markdownData.map((item) => (
-          <Route
-            key={item.id}
-            path={`/page/${item.slug}`}
-            render={() => (
-              <div key={item.id}>
-                {/* <h2>{item.title}</h2> */}
-                <ReactMarkdown>{item.content}</ReactMarkdown>
-              </div>
-            )}
-          />
-        ))}
-      </Switch>
-  );
-};
-
-export default App;
+// export default App;
