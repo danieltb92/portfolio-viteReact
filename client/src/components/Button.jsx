fedb154128fa97bc2853ec983e30f22c3bffb035
danieltb92 <daniel.tunjano92@gmail.com>
2024-04-25 11:43:59 -0500

    Fixed-Button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,13 @@
+import { PropTypes } from 'prop-types'
+
 function Button ({ text }){
     return (
         <a className="button w-fit" href="https://drive.google.com/file/d/18oWUDMiA1A_97Tim8I-doJrXEq76lNwP/view?usp=sharing" download target="_blank" rel="noopener" >
             {text}
         </a>
     )
-}
+} 
+Button.propTypes = {
+    text: PropTypes.string.isRequired
+};
 export default Button;
